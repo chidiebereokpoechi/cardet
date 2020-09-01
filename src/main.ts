756811a473b4d118c01dfe827e075850482ab514
Chidiebere Okpoechi <chidiebere.okpoechi@gmail.com>
2020-09-01 20:23:43 +0100

    Remove test code

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { NestFactory } from '@nestjs/core'
 import { HttpResponseInterceptor } from 'cardet/util'
 import { get } from 'config'
 import { AppModule } from './app.module'
-import { testEngine } from './engine/test-engine'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -22,9 +21,4 @@ async function bootstrap() {
   await app.listen(get<number>('app.port'))
 }
 
-function runTest() {
-  testEngine()
-}
-
 bootstrap()
-// runTest()
